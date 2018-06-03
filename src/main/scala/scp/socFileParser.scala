96ebfb770312a38ad2c856e94a41d602fdb00cc1
pietro <battilanap@gmail.com>
2018-06-03 22:19:08 +0200

    parsing edges and nodes in SOC format

--- src/main/scala/scp/socFileParser.scala
+++ src/main/scala/scp/socFileParser.scala
@@ -5,15 +5,17 @@ import org.apache.spark.rdd.RDD
 
 class socFileParser(filename: String) {
 
-  // (RDD[Map[Int, Int]], RDD[Set[Int]])
-  def parse(): Unit  = {
+  def parse(): (RDD[Map[Int, Int]], RDD[Int])  = {
     val sc: SparkContext = SparkContext.getOrCreate()
 
     val edges = sc.textFile(filename).filter(line => !line.startsWith("#")).map(line => line.split("\t")).map(nodes => Map(nodes(0).toInt -> nodes(1).toInt))
-    println("Found ", edges.count(), " edges in ", filename)
+    println("Parsed ", edges.count(), " edges in ", filename)
 
-    val nodes =
-    println("Found ", nodes.size, " nodes in ", filename)
+    val nodesSet: Set[Int] = sc.textFile(filename).filter(line => !line.startsWith("#")).flatMap(line => line.split("\t")).distinct.map(node => node.toInt).collect().toSet
+    val nodes = sc.parallelize(nodesSet.toSeq)
+    println("Parsed ", nodesSet.size, " nodes in ", filename)
+
+    (edges, nodes)
   }
 
 }
